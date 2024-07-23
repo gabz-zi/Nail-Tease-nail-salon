ef5408d8f6a9b7bb3fd68ec779045ec086572156
gabz-zi <gabriellla_koleva@abv.bg>
2024-07-24 00:11:44 +0300

    delete

--- src/main/java/com/nailSalon/model/entity/Category.java
+++
@@ -1,5 +0,0 @@
-package com.nailSalon.model.entity;
-
-public enum Category {
-    GELISH, GEL, PEDICURE, THERAPY
-}
--- src/main/java/com/nailSalon/model/enums/UserRoleEnum.java
+++
@@ -1,5 +0,0 @@
-package com.nailSalon.model.enums;
-
-public enum UserRoleEnum {
-    OWNER, MANAGER, EMPLOYEE, USER
-}
