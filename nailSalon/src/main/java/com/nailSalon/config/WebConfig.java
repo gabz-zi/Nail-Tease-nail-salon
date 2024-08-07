a49334d3192b6e8fbe268094d8c18ed77d07e7d8
gabz-zi <gabriellla_koleva@abv.bg>
2024-08-07 16:12:07 +0300

    i18n of navbar

--- nailSalon/src/main/java/com/nailSalon/config/WebConfig.java
+++ nailSalon/src/main/java/com/nailSalon/config/WebConfig.java
@@ -2,10 +2,22 @@ package com.nailSalon.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    private final LocaleChangeInterceptor localeChangeInterceptor;
+
+    public WebConfig(LocaleChangeInterceptor localeChangeInterceptor) {
+        this.localeChangeInterceptor = localeChangeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor);
+    }
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
